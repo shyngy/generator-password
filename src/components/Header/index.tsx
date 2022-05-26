b2b54df6eed1adf20cf3c6a1420c2fae0703514a
Shynygyskhan <shynygyskhan.bokikhan@gmail.com>
2022-05-26 16:45:53 +0600

    add header icon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import styles from './Header.module.scss';
-
+import headerImage from '../../../assets/images/header-icon.svg';
 const Header = () => {
   return (
     <div className={styles.headerWrapper}>
-      <img
-        className={styles.headerIcon}
-        src="./assets/images/header-icon.svg"
-      />
+      <img className={styles.headerIcon} src={headerImage} />
     </div>
   );
 };
